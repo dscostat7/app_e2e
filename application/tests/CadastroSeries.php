e634e0d3e556b464036719896ef104ea8099d513
Diego Souza <50335837+dscostat7@users.noreply.github.com>
2022-07-20 18:33:46 -0300

    Update CadastroSeries.php

--- application/tests/CadastroSeries.php
+++ application/tests/CadastroSeries.php
@@ -8,20 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 class CadastroSeriesTest extends TestCase
 {
-    public function testCadastrarNovaSerieDeveRedirecionarParaLista()
-    {
-        // Arrange
-        $host = 'http://localhost:4444/wd/hub';
-        $driver = RemoteWebDriver::create($host, DesiredCapabilities::chrome());
-        $driver->get('http://0.0.0.0:8080/adicionar-serie');
-
-        $driver->findElement(WebDriverBy::id('email'))->sendKeys('email@example.com');
-        $driver->findElement(WebDriverBy::id('password'))->sendKeys('123')->submit();
-
-        $driver->get('http://0.0.0.0:8080/adicionar-serie');
-
-        // Act
-        $inputNome = $driver->findElement(WebDriverBy::id('nome'));
+    public function testCadastrarNovaSer
         $inputGenero = $driver->findElement(WebDriverBy::id('genre'));
         $inputTemporadas = $driver->findElement(WebDriverBy::id('qtd_temporadas'));
         $inputEpisodios = $driver->findElement(WebDriverBy::id('ep_por_temporada'));
