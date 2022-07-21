fb62beceff414c36dd85e13167c174c5307efdd3
Diego Souza <50335837+dscostat7@users.noreply.github.com>
2022-07-21 18:09:14 -0300

    Create PaginaLogin.php

---
+++ application/tests/PageObject/PaginaLogin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace application\Tests\PageObject;
+
+use Facebook\WebDriver\WebDriver;
+use Facebook\WebDriver\WebDriverBy;
+
+class PaginaLogin
+{
+    private WebDriver $driver;
+
+    public function __construct(WebDriver $driver)
+    {
+        $this->driver = $driver;
+    }
+
+    public function realizaLoginCom(string $email, string $senha): void
+    {
+        $this->driver->get('http://0.0.0.0:8080/entrar');
+
+        $this->driver
+            ->findElement(WebDriverBy::id('email'))
+            ->sendKeys($email);
+
+        $this->driver
+            ->findElement(WebDriverBy::id('password'))
+            ->sendKeys($senha)
+            ->submit();
+    }
+}
